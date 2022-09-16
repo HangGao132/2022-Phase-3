0e52f78291d382dc986aea2ddd791e0e7a1433dd
HangGao132 <80610862+HangGao132@users.noreply.github.com>
2022-09-16 21:45:38 +1200

    data science model training and validation

--- back-end/GameApi/GameApi.Domain/Game/DTO/GameInstanceDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GameApi.Domain.Game.DTO;
-public class GameInstanceDTO
-{
-    public int UserId { get; set; }
-
-    public int RowSize { get; set; }
-
-    public int ColumnSize { get; set; }
-}
--- back-end/GameApi/GameApi.Domain/Game/DTO/GameStepDTO.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GameApi.Domain.Game.DTO;
-public class GameStepDTO
-{
-    public int GameInstanceId { get; set; }
-
-    public string Move { get; set; } = "";
-}
